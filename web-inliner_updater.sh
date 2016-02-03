069e4c2ed44335a6a2dcfdcf9009a7a20947a0c8
Alberto <alberto.vaccari@smarp.com>
2016-02-03 13:50:24 +0200

    updated updater scripts

--- web-inliner_updater.sh
+++ web-inliner_updater.sh
@@ -8,24 +8,31 @@ fi
 DOCKER="web-inliner"
 VERSION=$1
 
+printf '========================================\n'
 printf "# Updating to $VERSION...\n"
+printf '========================================\n'
 
 # Stopping and removing old container
 printf '# Removing old container...\n'
 docker rm -f $DOCKER
 printf '# Old container removed!\n'
+printf '========================================\n'
 
 # Remove old image
 printf '# Removing old image...\n'
 docker rmi $(docker images | grep $DOCKER | awk {'print $3'})
 printf '# Old image removed!\n'
+printf '========================================\n'
 
 printf '# Pulling new image...\n'
 docker pull smarp/$DOCKER:$VERSION
 printf '# New image pulled!\n'
+printf '========================================\n'
 
 docker run -d -p 17654:17654 -p 18765:18765 --name $DOCKER smarp/$DOCKER:$VERSION
 
+printf '========================================\n'
 printf "# Update to $VERSION complete!\n"
 
+printf '========================================\n'
 docker rmi $(docker images | awk {'print $3'})
